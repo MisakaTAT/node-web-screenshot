2d6f845244360351727695cc300f407d174dfc3c
MisakaTAT <i@mikuac.com>
2021-08-19 16:54:36 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # node-web-screenshot
-基于puppeteer与fastify的web截图服务
+基于`Puppeteer`与`Fastify`的Web截图服务
+
+# QuickStart
+
+## Clone project
+```
+git clone https://github.com/MisakaTAT/node-web-screenshot.git
+```
+
+## Project setup
+```
+npm install
+```
+
+## Running project
+```
+node index.js
+```
+
+## Usening
+```shell
+curl "http://127.0.0.1:3000" -H "Content-Type: application/json" -d "{\"url\":\"https://mikuac.com\"}" -X POST
+```
+Screenshot of the specified element
+```json
+{
+    "url": "https://mikuac.com",
+    "selector": "#aside-user > div > div > a > span > img"
+}
+```
+Full screenshot
+```json
+{
+    "url": "https://mikuac.com"
+}
+```
---
+++ index.js
@@ -0,0 +1,67 @@
+/**
+ * 基于puppeteer与fastify的web截图服务
+ * @author Zero
+ */
+const fastify = require('fastify')({
+  logger: false,
+});
+const puppeteer = require('puppeteer');
+
+async function webScreenShot(url, selector) {
+  let browser = null;
+  try {
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+      deviceScaleFactor: 1,
+    });
+
+    await page.goto(url, { waitUntil: 'load', timeout: 10000 });
+
+    if (selector) {
+      // screenshot of the specified element
+      let element = await page.$(selector);
+      const buffer = await element.screenshot();
+      return { code: 200, status: 'success', buffer: buffer };
+    } else {
+      // full screenshot
+      const buffer = await page.screenshot({ fullPage: true });
+      return { code: 200, status: 'success', buffer: buffer };
+    }
+  } catch (err) {
+    return { code: 500, status: 'failed', msg: err.message };
+  } finally {
+    await browser.close();
+  }
+}
+
+fastify.post('/', async function (request, reply) {
+  let url = request.body.url;
+  let selector = request.body.selector;
+
+  if (url) url = url.trim();
+  if (selector) selector = selector.trim();
+
+  if (!selector) selector = undefined;
+
+  // check url format
+  let checkUrl = /^(((ht|f)tps?):\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/.test(url);
+  if (!checkUrl) reply.code(400).send({ code: 400, status: 'failed', msg: 'url syntax error' });
+
+  const result = await webScreenShot(url, selector);
+  if (result.code == 200) {
+    reply.code(200).type('image/png').send(result.buffer);
+  } else {
+    reply.code(result.code).send(result);
+  }
+});
+
+fastify.listen(3000, (err, address) => {
+  console.log(`server is now listening on ${address}`);
+  if (err) {
+    console.log(err);
+  }
+});
